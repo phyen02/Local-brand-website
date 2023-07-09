a86b7f73a1dbaa7a394e1581b9376b4f61d13faa
redduck-02 <76261980+redduck-02@users.noreply.github.com>
2023-07-09 19:24:03 +0700

    update

---
+++ src/WebBanHangOnline/Migrations/202305160304132_InitialCreate.cs
@@ -0,0 +1,296 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tb_Adv",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(maxLength: 500),
+                        Image = c.String(maxLength: 500),
+                        Link = c.String(maxLength: 500),
+                        Type = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Category",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Alias = c.String(),
+                        Link = c.String(),
+                        Description = c.String(),
+                        SeoTitle = c.String(maxLength: 150),
+                        SeoDescription = c.String(maxLength: 250),
+                        SeoKeywords = c.String(maxLength: 150),
+                        IsActive = c.Boolean(nullable: false),
+                        Position = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Contact",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 150),
+                        Email = c.String(maxLength: 150),
+                        Website = c.String(),
+                        Message = c.String(maxLength: 4000),
+                        IsRead = c.Boolean(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_OrderDetail",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Order", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.tb_Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.tb_Order",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Code = c.String(nullable: false),
+                        CustomerName = c.String(nullable: false),
+                        Phone = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        Email = c.String(),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                        TypePayment = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Product",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 250),
+                        Alias = c.String(maxLength: 250),
+                        ProductCode = c.String(maxLength: 50),
+                        Description = c.String(),
+                        Detail = c.String(),
+                        Image = c.String(maxLength: 250),
+                        OriginalPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        PriceSale = c.Decimal(precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                        ViewCount = c.Int(nullable: false),
+                        IsHome = c.Boolean(nullable: false),
+                        IsSale = c.Boolean(nullable: false),
+                        IsFeature = c.Boolean(nullable: false),
+                        IsHot = c.Boolean(nullable: false),
+                        IsActive = c.Boolean(nullable: false),
+                        ProductCategoryId = c.Int(nullable: false),
+                        SeoTitle = c.String(maxLength: 250),
+                        SeoDescription = c.String(maxLength: 500),
+                        SeoKeywords = c.String(maxLength: 250),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_ProductCategory", t => t.ProductCategoryId, cascadeDelete: true)
+                .Index(t => t.ProductCategoryId);
+            
+            CreateTable(
+                "dbo.tb_ProductCategory",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Alias = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(),
+                        Icon = c.String(maxLength: 250),
+                        SeoTitle = c.String(maxLength: 250),
+                        SeoDescription = c.String(maxLength: 500),
+                        SeoKeywords = c.String(maxLength: 250),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        Modifiedby = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_ProductImage",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Image = c.String(),
+                        IsDefault = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.tb_SystemSetting",
+                c => new
+                    {
+                        SettingKey = c.String(nullable: false, maxLength: 50),
+                        SettingValue = c.String(maxLength: 4000),
+                        SettingDescription = c.String(maxLength: 4000),
+                    })
+                .PrimaryKey(t => t.SettingKey);
+            
+            CreateTable(
+                "dbo.ThongKes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ThoiGian = c.DateTime(nullable: false),
+                        SoTruyCap = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FullName = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.tb_ProductImage", "ProductId", "dbo.tb_Product");
+            DropForeignKey("dbo.tb_Product", "ProductCategoryId", "dbo.tb_ProductCategory");
+            DropForeignKey("dbo.tb_OrderDetail", "ProductId", "dbo.tb_Product");
+            DropForeignKey("dbo.tb_OrderDetail", "OrderId", "dbo.tb_Order");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.tb_ProductImage", new[] { "ProductId" });
+            DropIndex("dbo.tb_Product", new[] { "ProductCategoryId" });
+            DropIndex("dbo.tb_OrderDetail", new[] { "ProductId" });
+            DropIndex("dbo.tb_OrderDetail", new[] { "OrderId" });
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.ThongKes");
+            DropTable("dbo.tb_SystemSetting");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.tb_ProductImage");
+            DropTable("dbo.tb_ProductCategory");
+            DropTable("dbo.tb_Product");
+            DropTable("dbo.tb_Order");
+            DropTable("dbo.tb_OrderDetail");
+            DropTable("dbo.tb_Contact");
+            DropTable("dbo.tb_Category");
+            DropTable("dbo.tb_Adv");
+        }
+    }
+}
